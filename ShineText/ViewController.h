9008408887edc1195d12384895e4f5738e0ec4cf
QinMingchuan <413132340@qq.com>
2014-11-11 21:29:53 +0800

    Initial Commit

---
+++ ShineText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShineText
+//
+//  Created by QinMingChuan on 14/11/11.
+//  Copyright (c) 2014年 413132340@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShineText/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShineText
+//
+//  Created by QinMingChuan on 14/11/11.
+//  Copyright (c) 2014年 413132340@qq.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
